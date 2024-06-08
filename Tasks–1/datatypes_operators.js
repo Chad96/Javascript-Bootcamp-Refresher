e9e7dcb468a0177688397b8dba316bc7f9535987
Chad96 <chadrackndalamba@gmail.com>
2024-06-08 17:58:38 +0200

    datatypes

---
+++ Tasks–1/datatypes_operators.js
@@ -0,0 +1,25 @@
+// Exercise 1: Numbers
+
+// 1. Declare a number variable and assign it a value.
+let num1 = 25;
+
+// 2. Declare a number variable and assign it a floating number.
+let num2 = 12.5;
+
+// 3. Perform addition, subtraction, multiplication, division, modulus and exponentiation with number variable and another number.
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+let exponentiation = num1 ** 2; // num1 raised to the power of 2
+
+// 4. Print all your variables.
+console.log("num1:", num1);
+console.log("num2:", num2);
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log("Modulus:", modulus);
+console.log("Exponentiation:", exponentiation);
