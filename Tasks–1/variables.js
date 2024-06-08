08555af9c6753b45849b1b02b64eea798ddb877a
Chad96 <chadrackndalamba@gmail.com>
2024-06-08 17:37:57 +0200

    variable declaration

---
+++ Tasks–1/variables.js
@@ -0,0 +1,15 @@
+// Exercise 1: Variable Declaration
+
+// 1. Declare a variable ‘name’ and assign your name to it.
+let name = "Chadrack";
+
+// 2. Declare a variable ‘age’ and assign your age to it.
+let age = 28;
+
+// 3. Declare a variable ‘isStudent’ and assign it to a boolean value indicating whether you are a student or not.
+let isStudent = true;
+
+// 4. Print all three variables.
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
